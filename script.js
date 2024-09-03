d3d2953dde4f1e6219ff10c5b7a1eb4179a56782
rakawai <heraagatez@gmail.com>
2024-09-03 11:25:51 +0700

    add via upload
    ini commit yang pertama

---
+++ script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwipper",{
+    loop: true,
+    centeredSlides: true,
+    autoplay:{
+    disabledonINteraction: false,
+},
+breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    480: {
+        slidesPerView: 1,
+    },
+    480: {
+        slidesPerView: 1,
+    }},
+    480: {
+        slidesPerView: 1,
+    },
+    
+});   
